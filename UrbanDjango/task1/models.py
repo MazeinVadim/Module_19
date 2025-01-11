0af827527705757750790f479d0c1033a536ce32
Вадим <kvady@mail.ru>
2025-01-11 16:00:32 +0300

    Module_19_DZ_1

---
+++ UrbanDjango/task1/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=255)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyers = models.ManyToManyField(Buyer)  # Изменил здесь
+
+    def __str__(self):
+        return self.title
